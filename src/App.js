9556fd4bbade9aed8696dc27c62bf05bcea48b1e
Erhan Ertem <eertem@gmail.com>
2023-07-03 14:17:36 +0300

    Furnish UI for start state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Main from './Main'
 import Loader from './Loader'
 import Error from './Error'
 import StartScreen from './StartScreen'
+import Question from './Question'
 
 const initialState = {
 	questions: [],
@@ -24,6 +25,11 @@ function reducer(state, action) {
 			return {
 				...state,
 				status: 'error',
+			}
+		case 'start':
+			return {
+				...state,
+				status: 'active',
 			}
 		default:
 			throw new Error('Action is unknown')
@@ -49,7 +55,10 @@ export default function App() {
 			<Main>
 				{status === 'loading' && <Loader />}
 				{status === 'error' && <Error />}
-				{status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+				{status === 'ready' && (
+					<StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+				)}
+				{status === 'active' && <Question />}
 			</Main>
 		</div>
 	)
