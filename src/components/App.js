719f06567e30acd21e1c57bf10aacfc947dc4531
Erhan Ertem <eertem@gmail.com>
2023-07-03 18:27:36 +0300

    Handling answer to question

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,8 @@ const initialState = {
 	//Possible status: loading, error, ready, active, finished
 	status: 'loading',
 	index: 0,
+	answer: null,
+	points: 0,
 }
 
 function reducer(state, action) {
@@ -31,6 +33,17 @@ function reducer(state, action) {
 				...state,
 				status: 'active',
 			}
+		case 'newAnswer':
+			const question = state.questions.at(state.index)
+
+			return {
+				...state,
+				answer: action.payload,
+				points:
+					action.paylod === question.correctOption
+						? state.points + question.points
+						: state.points,
+			}
 		default:
 			throw new Error('Action is unknown')
 	}
@@ -38,7 +51,7 @@ function reducer(state, action) {
 
 export default function App() {
 	// const [state, dispatch] = useReducer(reducer, initialState)
-	const [{ questions, status, index }, dispatch] = useReducer(
+	const [{ questions, status, index, answer }, dispatch] = useReducer(
 		reducer,
 		initialState,
 	)
@@ -61,7 +74,13 @@ export default function App() {
 				{status === 'ready' && (
 					<StartScreen numQuestions={numQuestions} dispatch={dispatch} />
 				)}
-				{status === 'active' && <Question question={questions[index]} />}
+				{status === 'active' && (
+					<Question
+						question={questions[index]}
+						dispatch={dispatch}
+						answer={answer}
+					/>
+				)}
 			</Main>
 		</div>
 	)
