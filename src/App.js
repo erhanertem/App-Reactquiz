57aa12c593b6506dd5bf6b83a4ac6d60e313979c
Erhan Ertem <eertem@gmail.com>
2023-07-03 13:01:53 +0300

    Establish a useReducer hook for data fetching

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import { useEffect } from 'react'
+import { useEffect, useReducer } from 'react'
 import Header from './Header'
 import Main from './Main'
 
+const initialState = {
+	questions: [],
+
+	//Possible status: loading, error, ready, active, finished
+	status: 'loading',
+}
+
+function reducer(state, action) {
+	switch (action.type) {
+		case 'dataReceived':
+			return {
+				...state,
+				question: action.payload,
+			}
+		case 'dataFailed':
+			return {
+				...state,
+				status: 'error',
+			}
+		default:
+			throw new Error('Action is unknown')
+	}
+}
+
 export default function App() {
+	const [state, dispatch] = useReducer(reducer, initialState)
+
 	useEffect(function () {
 		fetch('http://localhost:9000/questions')
 			.then(res => res.json())
-			.then(data => console.log(data))
-			.catch(err => console.error('Error'))
+			.then(data => dispatch({ type: 'dataReceived', payload: data }))
+			.catch(err => dispatch({ type: 'dataFailed' }))
 	}, [])
 	return (
 		<div className="app">
