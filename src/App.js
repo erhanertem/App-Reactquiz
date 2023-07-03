d2ce31bbddd432b54c9e67702b56c402d7c40455
Erhan Ertem <eertem@gmail.com>
2023-07-03 12:50:56 +0300

    Create a json server + establish the project boiler plate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import DateCounter from './DateCounter'
+import { useEffect } from 'react'
+import Header from './Header'
+import Main from './Main'
 
 export default function App() {
+	useEffect(function () {
+		fetch('http://localhost:9000/questions')
+			.then(res => res.json())
+			.then(data => console.log(data))
+			.catch(err => console.error('Error'))
+	}, [])
 	return (
-		<div>
-			<DateCounter />
+		<div className="app">
+			<Header />
+			<Main>
+				<p>1/15</p>
+				<p>Question?</p>
+			</Main>
 		</div>
 	)
 }
