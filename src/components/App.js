749c5a76a225038d6a4c4f881ad7c6498724a0a1
Erhan Ertem <eertem@gmail.com>
2023-07-03 23:07:32 +0300

    Implement next question button

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Loader from './Loader'
 import Error from './Error'
 import StartScreen from './StartScreen'
 import Question from './Question'
+import NextButton from './NextButton'
 
 const initialState = {
 	questions: [],
@@ -44,6 +45,12 @@ function reducer(state, action) {
 						? state.points + question.points
 						: state.points,
 			}
+		case 'nextQuestion':
+			return {
+				...state,
+				index: state.index + 1,
+				answer: null,
+			}
 		default:
 			throw new Error('Action is unknown')
 	}
@@ -75,11 +82,14 @@ export default function App() {
 					<StartScreen numQuestions={numQuestions} dispatch={dispatch} />
 				)}
 				{status === 'active' && (
-					<Question
-						question={questions[index]}
-						dispatch={dispatch}
-						answer={answer}
-					/>
+					<>
+						<Question
+							question={questions[index]}
+							dispatch={dispatch}
+							answer={answer}
+						/>
+						<NextButton dispatch={dispatch} answer={answer} />
+					</>
 				)}
 			</Main>
 		</div>
