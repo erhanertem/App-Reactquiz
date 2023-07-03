5b8a5a6b309f4e1904eb699c2f367008604646be
Erhan Ertem <eertem@gmail.com>
2023-07-03 13:38:39 +0300

    Furnish UI for loading, error and ready states

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from 'react'
 import Header from './Header'
 import Main from './Main'
+import Loader from './Loader'
+import Error from './Error'
+import StartScreen from './StartScreen'
 
 const initialState = {
 	questions: [],
@@ -14,7 +17,8 @@ function reducer(state, action) {
 		case 'dataReceived':
 			return {
 				...state,
-				question: action.payload,
+				questions: action.payload,
+				status: 'ready',
 			}
 		case 'dataFailed':
 			return {
@@ -27,7 +31,11 @@ function reducer(state, action) {
 }
 
 export default function App() {
-	const [state, dispatch] = useReducer(reducer, initialState)
+	// const [state, dispatch] = useReducer(reducer, initialState)
+	const [{ questions, status }, dispatch] = useReducer(reducer, initialState)
+
+	const numQuestions = questions.length
+	console.log(questions)
 
 	useEffect(function () {
 		fetch('http://localhost:9000/questions')
@@ -39,8 +47,9 @@ export default function App() {
 		<div className="app">
 			<Header />
 			<Main>
-				<p>1/15</p>
-				<p>Question?</p>
+				{status === 'loading' && <Loader />}
+				{status === 'error' && <Error />}
+				{status === 'ready' && <StartScreen numQuestions={numQuestions} />}
 			</Main>
 		</div>
 	)
