019fc2773b7f18f21344b7dff5c1652e552ccb2e
Erhan Ertem <eertem@gmail.com>
2023-07-13 09:39:25 +0300

    Refactor using Context API

--- src/components/App.js
+++ src/components/App.js
@@ -10,100 +10,10 @@ import Progress from './Progress'
 import FinishScreen from './FinishScreen'
 import Footer from './Footer'
 import Timer from './Timer'
-
-const SECS_PER_QUESTION = 10
-
-const initialState = {
-	questions: [],
-	//Possible status: loading, error, ready, active, finished
-	status: 'loading',
-	index: 0,
-	answer: null,
-	points: 0,
-	highscore: 0,
-	secondsRemaining: null,
-}
-
-function reducer(state, action) {
-	switch (action.type) {
-		case 'dataReceived':
-			return {
-				...state,
-				questions: action.payload,
-				status: 'ready',
-			}
-		case 'dataFailed':
-			return {
-				...state,
-				status: 'error',
-			}
-		case 'start':
-			return {
-				...state,
-				status: 'active',
-				secondsRemaining: state.questions.length * SECS_PER_QUESTION,
-			}
-		case 'newAnswer':
-			const question = state.questions.at(state.index)
-
-			return {
-				...state,
-				answer: action.payload,
-				points:
-					action.payload === question.correctOption
-						? state.points + question.points
-						: state.points,
-			}
-		case 'nextQuestion':
-			return {
-				...state,
-				index: state.index + 1,
-				answer: null,
-			}
-		case 'finish':
-			return {
-				...state,
-				status: 'finished',
-				highscore:
-					state.points > state.highscore ? state.points : state.highscore,
-			}
-		case 'restart':
-			return {
-				...initialState,
-				questions: state.questions,
-				status: 'ready',
-			}
-		case 'tick':
-			return {
-				...state,
-				secondsRemaining: state.secondsRemaining - 1,
-				status: state.secondsRemaining === 0 ? 'finished' : state.status,
-			}
-
-		default:
-			throw new Error('Action is unknown')
-	}
-}
+import { useQuiz } from '../contexts/QuizContext'
 
 export default function App() {
-	// const [state, dispatch] = useReducer(reducer, initialState)
-	const [
-		{ questions, status, index, answer, points, highscore, secondsRemaining },
-		dispatch,
-	] = useReducer(reducer, initialState)
-
-	const numQuestions = questions.length
-	const maxPossiblePoints = questions.reduce(
-		(prev, cur) => prev + cur.points,
-		0,
-	)
-
-	useEffect(function () {
-		fetch('http://localhost:9000/questions')
-			.then(res => res.json())
-			.then(data => dispatch({ type: 'dataReceived', payload: data }))
-			.catch(err => dispatch({ type: 'dataFailed' }))
-	}, [])
+	const { status } = useQuiz()
 	return (
 		<div className="app">
 			<Header />
@@ -111,39 +21,43 @@ export default function App() {
 				{status === 'loading' && <Loader />}
 				{status === 'error' && <Error />}
 				{status === 'ready' && (
-					<StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+					<StartScreen
+					// numQuestions={numQuestions} dispatch={dispatch}
+					/>
 				)}
 				{status === 'active' && (
 					<>
 						<Progress
-							numQuestions={numQuestions}
-							index={index}
-							points={points}
-							maxPossiblePoints={maxPossiblePoints}
-							answer={answer}
+						// numQuestions={numQuestions}
+						// index={index}
+						// points={points}
+						// maxPossiblePoints={maxPossiblePoints}
+						// answer={answer}
 						/>
 						<Question
-							question={questions[index]}
-							dispatch={dispatch}
-							answer={answer}
+						// question={questions[index]}
+						// dispatch={dispatch}
+						// answer={answer}
 						/>
 						<Footer>
-							<Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+							<Timer
+							// dispatch={dispatch} secondsRemaining={secondsRemaining}
+							/>
 							<NextButton
-								dispatch={dispatch}
-								answer={answer}
-								numQuestions={numQuestions}
-								index={index}
+							// dispatch={dispatch}
+							// answer={answer}
+							// numQuestions={numQuestions}
+							// index={index}
 							/>
 						</Footer>
 					</>
 				)}
 				{status === 'finished' && (
 					<FinishScreen
-						dispatch={dispatch}
-						points={points}
-						maxPossiblePoints={maxPossiblePoints}
-						highscore={highscore}
+					// dispatch={dispatch}
+					// points={points}
+					// maxPossiblePoints={maxPossiblePoints}
+					// highscore={highscore}
 					/>
 				)}
 			</Main>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,15 @@
+import { useQuiz } from '../contexts/QuizContext'
 import Options from './Options'
 
-function Question({ question, dispatch, answer }) {
-	console.log(question)
+function Question() {
+	const { questions, index } = useQuiz()
+	const question = questions[index]
+	// console.log(question)
+
 	return (
 		<div>
 			<h4>{question.question}</h4>
-			<Options question={question} dispatch={dispatch} answer={answer} />
+			<Options question={question} />
 		</div>
 	)
 }
