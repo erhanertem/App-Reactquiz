111c390c2c7595d081b4813d0ddbfae8b588aa48
Erhan Ertem <eertem@gmail.com>
2024-09-04 16:57:30 +0300

    Add server for json-server, add package scripts for build and prod

--- src/components/App.js
+++ src/components/App.js
@@ -81,8 +81,7 @@ function App() {
   const hasAnswered = answer !== null;
 
   useEffect(() => {
-    // fetch('http://localhost:8000/questions')
-    fetch('https://react-quiz-erhan-ertem.onrender.com/questions')
+    fetch('http://localhost:8000/questions')
       .then((res) => res.json())
       .then((data) => dispatch({ type: 'dataReceived', payload: data }))
       .catch((err) => dispatch({ type: 'dataFailed' }));
---
+++ src/server.js
@@ -0,0 +1,21 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, '../data/questions.json')); // Your database file
+const middlewares = jsonServer.defaults();
+
+// Add custom middleware for CORS
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Allow any origin
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+// Use default router
+server.use(router);
+server.listen(8000, () => {
+  console.log('JSON Server is running');
+});
